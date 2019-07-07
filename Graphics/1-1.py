6494f77379d5a40aaa0c3bd3959d1bcb528faba1
Rajeev Gupta <rajeev@Rajeevs-MacBook-Pro.local>
2019-07-07 16:10:26 -0700

    Added Connect4

--- Graphics/1-1.py
+++ Graphics/1-1.py
@@ -1,16 +1,15 @@
 import pygame
-import random
 
 WIDTH = 360
 HEIGHT = 480
 FPS = 30
 
-BLACK = [0,0,0]
+BLACK = [1, 0, 0]
 
 pygame.init()
 pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH,HEIGHT ))
-pygame.display.set_caption("My Game") 
+screen = pygame.display.set_mode((WIDTH, HEIGHT ))
+pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
@@ -24,5 +23,6 @@ while running:
     screen.fill(BLACK)
     all_sprites.draw(screen)
     pygame.display.flip()
+    pygame.display.update()
 
 pygame.quit()
